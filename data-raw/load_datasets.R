f188f7d3a748d6024d6146359e5fa61994512097
flowerside <flowerside@gmail.com>
2023-07-12 10:03:25 +0400

    Minor changes

--- data-raw/load_datasets.R
+++ data-raw/load_datasets.R
@@ -1,9 +1,10 @@
 library(iotc.base.common.data)
 
 NC_RAW = NC.raw()
-NC_SCI = NC.est()
-NC_RSD = NC.raised()
-
 usethis::use_data(NC_RAW, overwrite = TRUE)
+
+NC_SCI = NC.est()
 usethis::use_data(NC_SCI, overwrite = TRUE)
+
+NC_RSD = NC.raised()
 usethis::use_data(NC_RSD, overwrite = TRUE)
