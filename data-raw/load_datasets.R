412c33a3b92616736d7525447b6f3523f4d98396
flowerside <flowerside@gmail.com>
2023-07-12 15:07:46 +0400

    Switching to non-factorised data

--- data-raw/load_datasets.R
+++ data-raw/load_datasets.R
@@ -1,10 +1,10 @@
 library(iotc.base.common.data)
 
-NC_RAW = NC.raw()
+NC_RAW = NC.raw(factorize_results = FALSE)
 usethis::use_data(NC_RAW, overwrite = TRUE)
 
-NC_SCI = NC.est()
+NC_SCI = NC.est(factorize_results = FALSE)
 usethis::use_data(NC_SCI, overwrite = TRUE)
 
-NC_RSD = NC.raised()
+NC_RSD = NC.raised(factorize_results = FALSE)
 usethis::use_data(NC_RSD, overwrite = TRUE)
