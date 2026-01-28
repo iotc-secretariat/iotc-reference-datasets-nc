a5fa8040b8a3e29dcb0856f78748dd14c4b6bb04
manuchassot <manu.chassot@gmail.com>
2026-01-28 15:39:32 +0400

    Added note on converting legacy - new formats

--- build_and_upload.R
+++ build_and_upload.R
@@ -12,6 +12,10 @@ source("./data-raw/01_load_datasets.R")
 # Generate the datasets ####
 source("./data-raw/02_export_datasets.R")
 
+# TO DO - create converters between legacy and new formats
+# View [Miguel].[dbo].[1NC_WebPageFILE]: RAW | "Z:/Public datasets/2025-01-14/IOTC-DATASETS-2025-01-14-NC-ALL_1950-2023.xlsx"
+# Table [IOTDB].[dbo].[est_NCdissag]: SCI | "Z:/Public datasets/2025-01-14/IOTC-DATASETS-2025-01-14-NC-SCI_1950-2023.xlsx"
+
 # Generate dataset description ###
 source("./data-raw/03_export_metadata.R")
 
